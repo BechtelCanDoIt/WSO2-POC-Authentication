cfae01ddc9c13da885f0562ff3abd6fce82a2420
Scott Bechtel <Scott.Bechtel.contractor@hilton.com>
2023-12-11 21:03:17 -0600

    Further refinement of folder references

--- wso2-config/am/entrypoint.sh
+++ wso2-config/am/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pwd
-mv ../repository/conf/deployment.toml ../repository/conf/deployment.toml.orig
-cp ./deployment.toml ../repository/conf
+BASE_DIR="/home/wso2am-4.0.0/"
+POC_DIR="$BASE_DIR/poc"
+mv "$BASE_DIR/repository/conf/deployment.toml" "$BASE_DIR/repository/conf/deployment.toml.orig"
+cp "$POC_DIR/deployment.toml" "$BASE_DIR/repository/conf"
--- wso2-config/is/entrypoint.sh
+++ wso2-config/is/entrypoint.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
-pwd
+POC_DIR="/home/wso2is-5.11.0/poc"
+REPOS_DIR"/home/wso2carbon/wso2is-5.11.0/repository"
+CONF_DIR="$REPOS_DIR/conf"
+
+# Move original deployment.toml, if it exists
+if [ -f "$CONF_DIR/deployment.toml" ]; then
+    mv "$CONF_DIR/deployment.toml" "$CONF_DIR/deployment.toml.orig"
+fi
 mv ../repository/conf/deployment.toml ../repository/conf/deployment.toml.orig
 
 ### Copy items from poc folder to where they need to live in the server
-cp ./deployment.toml ../repository/conf
-cp ./rest-auth.properties ../repository/conf
+cp "$POC_DIR/deployment.toml" "$CONF_DIR/"
+cp "$POC_DIR/rest-auth.properties" "$CONF_DIR/"
 
-cp ./org.wso2.carbon.extension.identity.emailotp.common-4.0.6.jar ../repository/components/dropins
-cp ./org.wso2.carbon.extension.identity.smsotp.common-3.0.15.jar ../repository/components/dropins
-cp ./org.wso2.carbon.identity.oauth2.grant.rest.core-1.0.0.jar ../repository/components/dropins
-cp ./identity.oauth2.grant.auth.rest.handler-1.0.0.jar ../repository/components/libs
+cp "$POC_DIR/org.wso2.carbon.extension.identity.emailotp.common-4.0.6.jar" "$REPOS_DIR/components/dropins"
+cp "$POC_DIR/org.wso2.carbon.extension.identity.smsotp.common-3.0.15.jar" "$REPOS_DIR/components/dropins"
+cp "$POC_DIR/org.wso2.carbon.identity.oauth2.grant.rest.core-1.0.0.jar" "$REPOS_DIR/components/dropins"
+cp "$POC_DIR/identity.oauth2.grant.auth.rest.handler-1.0.0.jar" "$REPOS_DIR/components/libs"
 
-cp ./api#identity#authn#v1.war ../repository/deployment/server/webapps
+cp "$POC_DIR/api#identity#authn#v1.war" "$REPOS_DIR/deployment/server/webapps"
 
-cp ./SMSPublisher.xml ../repository/deployment/server/eventpublishers
+cp "$POC_DIR/SMSPublisher.xml" "$REPOS_DIR/deployment/server/eventpublishers"
 
 ### Add notification configs to IS
-echo "" >> ../repository/conf/sms/sms-templates-admin-config.xml
-echo "<configuration type="sendOTP" display="sendOTP" locale="en_US">" >> ../repository/conf/sms/sms-templates-admin-config.xml
-echo "    <body>Your One Time Password is : {{confirmation-code}}</body>" >> ../repository/conf/sms/sms-templates-admin-config.xml
-echo "</configuration>" >> ../repository/conf/sms/sms-templates-admin-config.xml
+# Add notification configs to IS, check if the directory exists
+if [ -d "$CONF_DIR/sms" ]; then
+    echo "<configuration type=\"sendOTP\" display=\"sendOTP\" locale=\"en_US\">" >> "$CONF_DIR/sms/sms-templates-admin-config.xml"
+    echo "    <body>Your One Time Password is : {{confirmation-code}}</body>" >> "$CONF_DIR/sms/sms-templates-admin-config.xml"
+    echo "</configuration>" >> "$CONF_DIR/sms/sms-templates-admin-config.xml"
+fi
 
 ### Per Directions: Restart server - to pick up configuration changes 
-../bin/wso2server.sh restart
+/home/wso2is-5.11.0/bin/wso2server.sh restart
